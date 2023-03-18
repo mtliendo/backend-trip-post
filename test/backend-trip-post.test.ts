5556f220c9682ec54f8bb6bb76c2e4650e098d29
focusotter <mtliendo@focusotter.com>
2023-03-18 13:06:03 -0500

    Initial commit

---
+++ test/backend-trip-post.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BackendTripPost from '../lib/backend-trip-post-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/backend-trip-post-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BackendTripPost.BackendTripPostStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
